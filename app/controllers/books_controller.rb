0b4f8ac00aaacb3747b2f5fd1c3ec421a93d7c9a
furukawakousuke <kendousyonan@gmail.com>
2022-08-28 11:07:44 +0000

    [bookers2]add

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,17 +11,27 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
+    if @book.save
     redirect_to books_path
+  else
+    render :new
   end
+end
 
   def show
     @book = Book.find(params[:id])
+    @book_new = Book.new
+    @user = current_user
   end
-
   def edit
     @book = Book.find(params[:id])
   end
+  
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to '/books'
+  end 
   
   private
   
